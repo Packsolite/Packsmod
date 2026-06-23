e766efa00366c37bdba56bda9bfb8bdbf24b1004
Packsolite <packsolite@gmail.com>
2026-06-23 22:30:17 +0200

    :sparkles: Add smash radio

--- src/main/java/eu/packsolite/packsmod/feature/radio/player/MusicStream.java
+++ src/main/java/eu/packsolite/packsmod/feature/radio/player/MusicStream.java
@@ -8,7 +8,8 @@ public enum MusicStream {
 	s1("I ❤ 2 Dance", "http://stream01.iloveradio.de/iloveradio2.mp3"),
 	s2("I ❤ Mashup", "http://stream01.iloveradio.de/iloveradio5.mp3"),
 	s3("HouseTime.FM", "http://mp3.stream.tb-group.fm/ht.mp3"),
-	s7("DREAM radio J-Pop", "https://kathy.torontocast.com:3060/;?type=http");
+	s7("DREAM radio J-Pop", "https://kathy.torontocast.com:3060/;?type=http"),
+	s8("Smash Radio", "https://radio.smashmc.eu/radio");
 
 	final String stream;
 	final String name;
